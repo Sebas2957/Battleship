2af7541417b17b6449652fa79546cde9c388b0ef
alejandro <pablo.gaviria@correounivalle.edu.co>
2025-12-13 22:28:17 -0500

    Arreglado el estiramiento en la última columna

--- src/main/java/org/example/Battleship/Views/PlacementView.java
+++ src/main/java/org/example/Battleship/Views/PlacementView.java
@@ -3,6 +3,7 @@ package org.example.Battleship.Views;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -11,6 +12,7 @@ import java.io.IOException;
  *
  * @author Javier Giraldo
  * @author Sebastian Niño
+ * @author Pablo Arias
  * @version 1.0
  */
 public class PlacementView {
@@ -28,7 +30,28 @@ public class PlacementView {
 
         stage = new Stage();
         stage.setTitle("Batalla Naval - Coloca tus barcos");
-        stage.setScene(new Scene(root, 600, 550));
+        // Tamaño: 1337x705 para coincidir con placement-view.fxml
+        stage.setScene(new Scene(root, 1337, 705));
+        stage.setResizable(false);
+    }
+
+    /**
+     * Overloaded constructor that sets an owner for proper centering.
+     *
+     * @param owner The owner Stage to which this window should be attached
+     * @throws IOException If FXML file cannot be loaded
+     */
+    public PlacementView(Stage owner) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/Battleship/placement-view.fxml"));
+        Parent root = loader.load();
+
+        stage = new Stage();
+        if (owner != null) {
+            stage.initOwner(owner);
+            stage.initModality(Modality.WINDOW_MODAL);
+        }
+        stage.setTitle("Batalla Naval - Coloca tus barcos");
+        stage.setScene(new Scene(root, 1337, 705));
         stage.setResizable(false);
     }
 
@@ -36,6 +59,8 @@ public class PlacementView {
      * Shows the placement window.
      */
     public void show() {
+        stage.sizeToScene();
         stage.show();
+        stage.centerOnScreen();
     }
 }
